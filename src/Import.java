fc10e798630eb3674bddbeb196858c3972ad3338
JUSTINAYA <2813970520@qq.com>
2023-10-28 22:53:10 +0800

    common

--- src/Import.java
+++ src/Import.java
@@ -20,10 +20,8 @@ public class Import {
             System.out.print(arr[i] + " ");
         }*/
 
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
 
-        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
